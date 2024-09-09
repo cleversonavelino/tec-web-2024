e9bbfe9c3525794cb27adadc26f6e862c7db9bc6
Avelino <cleverson.avelino@dell.com>
2024-09-09 19:32:54 -0300

    first api

--- turmab/index.js
+++ turmab/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var app = express();
-
-const port = 3000;
+app.use(express.json());
 
 app.use(express.static('./pages'));
 
-app.get('/hello', (req, res) => {
-    res.send('Hello, World! 123');
+const produtos = [];
+
+const router = express.Router();
+router.get('/api/produtos', (req, res) => {  
+    console.log('entrou no get');
+    res.status(200).json(produtos);
+});
+router.post('/api/produtos', (req, res) => {  
+    console.log('entrou no post');
+    console.log(req.body);
+
+    var produto = req.body;
+    produto.id = 1;
+
+    produtos.push(produto);
+    res.status(201).json(produto);
 });
 
+app.use(router);
+
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
