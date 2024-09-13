754606cda244a7eec300a92cbbfb466934118b3e
Avelino <cleverson.avelino@dell.com>
2024-09-12 21:32:20 -0300

    api rest

--- turmaa/index.js
+++ turmaa/index.js
@@ -4,11 +4,34 @@ var app = express();
 app.use(express.static('./pages'));
 
 const port = 3000;
+const router = express.Router();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello, World!');
+var produtos = [];
+
+router.get("/api/produtos", (request, response) => {    
+    response.status(200).json(produtos);
 });
 
+router.post("/api/produtos", (request, response) => {
+    const produto = request.body;
+    produto.id = produtos.length + 1;
+    produtos.push(produto);
+    response.status(201).json(produto);
+});
+
+router.delete("/api/produtos/:id", (request, response) => {
+    const id = request.params.id;
+    console.log(produtos);
+    produtos = produtos.filter(p => p.id != id);    
+    response.status(204).send("produtos excluído com sucesso");
+});
+
+//filter
+//reduce
+//map
+
+app.use(router);
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
