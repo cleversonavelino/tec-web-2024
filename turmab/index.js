0f0b5d5e2fbf55b789068a67043a11fa8aa53f0d
Avelino <cleverson.avelino@dell.com>
2024-09-30 19:36:39 -0300

    mysql

--- turmab/index.js
+++ turmab/index.js
@@ -4,40 +4,57 @@ app.use(express.json());
 
 app.use(express.static('./pages'));
 
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "35.202.15.50",
+    user: "root",
+    password: "123456"
+});
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 const produtos = [];
-const usuarios = [];
 
 let idUsuarios = 0;
 
 const router = express.Router();
-router.get('/api/produtos', (req, res) => { 
+router.get('/api/produtos', (req, res) => {
     res.status(200).json(produtos);
 });
-router.post('/api/produtos', (req, res) => {     
+router.post('/api/produtos', (req, res) => {
     var produto = req.body;
     produto.id = 1;
-    produtos.push(produto);    
+    produtos.push(produto);
     res.status(201).json(produto);
 });
 
-router.get('/api/usuarios', (req, res) => { 
-    res.status(200).json(usuarios);
+router.get('/api/usuarios', (req, res) => {
+    con.query("SELECT u.id, u.email, u.status FROM usuario u", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+    res.status(200).json({});
 });
-router.get('/api/usuarios/:id', (req, res) => { 
-    const id = req.param("id");
-    console.log(id);
-
-    const resultado = usuarios.filter(u => u.id == id);
-
-    res.status(200).json(resultado[0]);
-});
-router.post('/api/usuarios', (req, res) => {     
+router.post('/api/usuarios', (req, res) => {
     var usuario = req.body;
-    usuario.id = ++idUsuarios;
-    usuarios.push(usuario);    
+    var sql = `INSERT INTO usuario (email, senha, status) VALUES ('${usuario.email}', '${usuario.senha}',
+    '${usuario.status}')`;
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
     res.status(201).json(usuario);
 });
-router.delete('/api/usuarios/:id', (req, res) => { 
+router.get('/api/usuarios/:id', (req, res) => {
+    const id = req.param("id");
+    console.log(id);
+    const resultado = usuarios.filter(u => u.id == id);
+    res.status(200).json(resultado[0]);
+});
+router.delete('/api/usuarios/:id', (req, res) => {
     const id = req.param("id");
     console.log(id);
 
